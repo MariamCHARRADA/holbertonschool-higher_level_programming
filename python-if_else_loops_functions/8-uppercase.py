f23a0448677d10fa47bd202966233c6b2028ac94
MariamCHARRADA <charradamariam@yahoo.com>
2023-09-28 22:31:42 +0100

    increment i

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -3,5 +3,6 @@ def uppercase(str):
     for i in range(len(str)):
         if ord(i) in range(97, 123):
             print("{}".format(chr(ord(i) + 97)))
+            i += 1
         else:
             print("{}".format(chr(i)))
