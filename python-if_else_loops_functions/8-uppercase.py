6924bcc5f9b6862dcd2295c5ae2f0640aff48a96
MariamCHARRADA <charradamariam@yahoo.com>
2023-09-28 22:48:50 +0100

    change else

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -3,5 +3,4 @@ def uppercase(str):
     for ch in str:
         if 'a' <= ch <= 'z':
             print("{}".format(chr(ord(ch) - 32)), end="")
-        else:
-            print("{}".format(ch), end="")
+    print()
