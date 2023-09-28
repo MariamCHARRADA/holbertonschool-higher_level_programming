41d9daf5ac72e198a06be1cfa38c361a1f3bf03d
MariamCHARRADA <charradamariam@yahoo.com>
2023-09-28 22:38:18 +0100

    subtract 32

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -2,6 +2,6 @@
 def uppercase(str):
     for i in range(len(str)):
         if ord(i) in range(97, 123):
-            print("{}".format(chr(ord(i) - 32)))
+            print("{}".format(chr(ord(i) - 32)), end="")
         else:
-            print("{}".format(chr(i)))
+            print("{}".format(chr(i)), end="")
