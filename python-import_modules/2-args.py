aee439641a6d91b117da68f3626f7b13e7c21f43
MariamCHARRADA <charradamariam@yahoo.com>
2023-09-29 23:19:45 +0100

    if __name__ == __main__:

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-from sys import argv
-args = len(argv)
-if args == 2:
-    print("1 argument:")
-    print("1: {}".format(argv[1]))
-elif args == 1:
-    print("0 arguments.")
-else:
-    print("{} arguments:".format(args - 1))
-    for i in range(1, args):
-        print("{}: {}".format(i, argv[i]))
+if __name__ == "__main__":
+    from sys import argv
+    args = len(argv)
+    if args == 2:
+        print("1 argument:")
+        print("1: {}".format(argv[1]))
+    elif args == 1:
+        print("0 arguments.")
+    else:
+        print("{} arguments:".format(args - 1))
+        for i in range(1, args):
+            print("{}: {}".format(i, argv[i]))
