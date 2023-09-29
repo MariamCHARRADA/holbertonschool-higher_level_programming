80fb99b057a1c9eafe463f5fa248b146186442e1
MariamCHARRADA <charradamariam@yahoo.com>
2023-09-29 23:18:21 +0100

    fix for loop

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -7,6 +7,6 @@ if args == 2:
 elif args == 1:
     print("0 arguments.")
 else:
-    print("{} arguments:".format(args))
+    print("{} arguments:".format(args - 1))
     for i in range(1, args):
         print("{}: {}".format(i, argv[i]))
