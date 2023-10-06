1bb11df6e787364d9d063f0d04655333376da2c9
MariamCHARRADA <135614551+MariamCHARRADA@users.noreply.github.com>
2023-10-06 12:08:16 +0000

    fix cond

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if idx in my_list:
+    if idx in range(len(my_list)) and idx > 0:
         return my_list[idx]
     else:
         return None
