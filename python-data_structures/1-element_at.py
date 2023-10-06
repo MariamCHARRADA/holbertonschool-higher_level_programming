807b2d8f7b9518c976d15d8a46a374e01d10777b
MariamCHARRADA <135614551+MariamCHARRADA@users.noreply.github.com>
2023-10-06 12:15:14 +0000

    >=0

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if idx in range(len(my_list) + 1) and idx > 0:
+    if idx in range(len(my_list)) and idx >= 0:
         return my_list[idx]
     else:
         return None
