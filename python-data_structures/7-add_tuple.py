ff26496d2d1644fb02d9fb565c52f5fbd0d61427
MariamCHARRADA <135614551+MariamCHARRADA@users.noreply.github.com>
2023-10-07 11:01:30 +0000

    fix len<2 cases

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
+    while len(tuple_a) < 2:
+        tuple_a = tuple_a + (0,)
+    while len(tuple_b) < 2:
+        tuple_b = tuple_b + (0,)
     res = []
     for i in range(2):
         res.append(tuple_a[i] + tuple_b[i])
