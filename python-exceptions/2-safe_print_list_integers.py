7a10764d2445d8fd5b4eaad9a70b90a3cc77a540
MariamCHARRADA <charradamariam@yahoo.com>
2023-10-11 01:30:50 +0100

    code fix

---
+++ python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    try:
+        for i in range(x):
+            print("{:d}".format(my_list[i]))
+
+        return x
+    except TypeError, IndexError:
+        pass
+    
+    print()
+    return i
