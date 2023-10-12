69f5b72229680bcd60f9cfdbba85ab989835a654
MariamCHARRADA <charradamariam@yahoo.com>
2023-10-13 00:32:52 +0100

    swap

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -21,7 +21,7 @@ class Rectangle():
         '''width setter'''
         if not isinstance(value, int):
             raise TypeError('width must be an integer')
-        elif not value >= 0:
+        if value < 0:
             raise ValueError('width must be >=0')
         else:
             self.__width = value
@@ -36,7 +36,7 @@ class Rectangle():
         '''height setter'''
         if not isinstance(value, int):
             raise TypeError('height must be an integer')
-        elif not value >= 0:
+        if value < 0:
             raise ValueError('height must be >=0')
         else:
             self.__height = value
