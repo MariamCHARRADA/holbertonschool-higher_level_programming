94fd5ba4aac8a1d4806b2a5e25805ea708ff6f88
MariamCHARRADA <charradamariam@yahoo.com>
2023-10-12 11:11:34 +0100

    test

---
+++ python-classes/1-square.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""a class Square that defines a square by Private instance attribute: size"""
+class Square:
+    """A class that defines a square"""
+    def __init__(self, size):
+        self.__size = size
