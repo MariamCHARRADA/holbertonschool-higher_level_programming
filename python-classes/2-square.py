29b95f84ca92ccf178cabc8125f4146b001b770c
MariamCHARRADA <charradamariam@yahoo.com>
2023-10-12 11:20:07 +0100

    class docstring

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -7,6 +7,7 @@ size must be an integer, otherwise raise a TypeError exception with the message 
 if size is less than 0, raise a ValueError exception with the message size must be >= 0
 '''
 class Square:
+    '''a class that defines a square'''
     def __init__(self, size=0):
         if type(size)!= int:
             raise TypeError("size must be an integer")
