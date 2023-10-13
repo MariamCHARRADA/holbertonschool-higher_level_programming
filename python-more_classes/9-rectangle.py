e98e140b73075fad776a8d21eb3d5f86c7c09648
MariamCHARRADA <charradamariam@yahoo.com>
2023-10-13 03:25:48 +0100

    indent

--- python-more_classes/9-rectangle.py
+++ python-more_classes/9-rectangle.py
@@ -85,7 +85,5 @@ class Rectangle:
             return rect_2
 
     @classmethod
-    def square(cls, size=0):
-        '''returns a new Rectangle instance with width == height == size'''
-
+    def square(cls, size=0):  
         return cls(size, size)
