fe3fd02a6bf093af281082a5a4d59a43c28b583a
MariamCHARRADA <charradamariam@yahoo.com>
2023-10-13 22:04:36 +0100

    fix code

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -51,4 +51,4 @@ class Square:
         else:
             print("{}".format('\n' * self.__position[1]), end='')
             for i in range(self.__size):
-                print(f"{' ' * self.__position[0]}{'#' * self.__size}")
+                print("{}{}".format(' ' * self.__position[0], '#' * self.__size))
