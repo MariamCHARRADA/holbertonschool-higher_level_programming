1dee40612c4860c61a458d9afbfcd1c0fce59fed
MariamCHARRADA <charradamariam@yahoo.com>
2023-10-13 21:47:35 +0100

    fix nested loops

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -49,9 +49,13 @@ class Square:
         """prints in stdout the square with the character #"""
         if self.__size == 0:
             print()
-            return
+
         else:
             for i in range(self.__position[1]):
                 print()
             for i in range(self.__size):
-                print(" " * self.__position[0] + "#" * self.__size)
+                for j in range(self.__position[0]):
+                    print(' ', end='')
+                for k in range(self.__size):
+                    print('#', end='')
+                print('')
