91c5cc7dc4687214ad8c94bdc19a9e3a0ba2125c
MariamCHARRADA <charradamariam@yahoo.com>
2023-10-13 21:34:51 +0100

    fix else code

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -50,6 +50,7 @@ class Square:
         if self.__size == 0:
             print()
         else:
-            print("\n" * self.__position[1], end="")
+            for i in range(self.__position[1]):
+                print()
             for i in range(self.__size):
-                print(f"{' ' * self.__position[0]}{'#' * self.__size}")
+                print(" " * self.__position[0] + "#" * self.__size)
