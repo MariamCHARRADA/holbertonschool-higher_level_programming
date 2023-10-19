be920a70bb33a5121e14bb5010d3d7766804bc5a
MariamCHARRADA <charradamariam@yahoo.com>
2023-10-19 17:42:02 +0100

    with open

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -8,6 +8,7 @@ load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 file = "add_item.json"
 args = sys.argv
 
-mylist = args[1:]
-save_to_json_file(mylist, file)
-load_from_json_file(file)
+with open(file, "a+", encoding="utf-8") as f:
+    my_list = args[1:]
+    save_to_json_file(my_list, file)
+    load_from_json_file(file)
