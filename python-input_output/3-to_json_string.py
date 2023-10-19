881919204ef0a85575e0ca31ffeef4d1f372a756
MariamCHARRADA <charradamariam@yahoo.com>
2023-10-19 16:10:27 +0100

    test

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""JSON representation of an object (string)"""
+
+
+def to_json_string(my_obj):
+    """returns the JSON representation of an object (string)"""
+    return json.loads(my_obj)
