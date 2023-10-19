4f2d57e6b5187a739ab7520fa47339625934a8ec
MariamCHARRADA <charradamariam@yahoo.com>
2023-10-19 03:19:14 +0100

    test

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,19 @@
+"""''Square class"""
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    """Square class"""
+
+    def __init__(self, size):
+        """initialize square size"""
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        """returns square area"""
+        return self.__size**2
+
+    def __str__(self):
+        '''returns square description'''
+        return "[Square] {:d}/{:d}".format(self.__size, self.__size)
