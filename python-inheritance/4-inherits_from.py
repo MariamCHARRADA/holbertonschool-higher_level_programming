fa17fcd2557da69d5b040ca4c04dffb38a854ada
MariamCHARRADA <charradamariam@yahoo.com>
2023-10-19 01:59:47 +0100

    not a_class

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -4,4 +4,4 @@
 
 def inherits_from(obj, a_class):
     """returns True if the object is exactly an instance of the specified class"""
-    return issubclass(type(obj), a_class)
+    return issubclass(type(obj), a_class) and type(obj) != a_class
