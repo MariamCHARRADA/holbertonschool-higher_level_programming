7c486437ac5e78ff4e29042675393758003f581a
MariamCHARRADA <charradamariam@yahoo.com>
2023-10-19 18:37:18 +0100

    test

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """creates an Object from a “JSON file”"""
 import sys
-from os.path import isfile
 
 
 save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
@@ -10,9 +9,9 @@ load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 filename = "add_item.json"
 args = sys.argv
 
-if isfile(filename):
+try:
     my_list = load_from_json_file(filename)
-else:
+except:
     my_list = []
 
 my_list.append(args[1:])
