b9bae04474492a29afa2f1e51fe1699e59eae059
MariamCHARRADA <charradamariam@yahoo.com>
2023-10-19 02:38:36 +0100

    test

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,8 +1,28 @@
 #!/usr/bin/python3
+"""BaseGeography class"""
+
 
 class BaseGeometry:
-    
-    class Rectangle(BaseGeometry):
-        def __init__(self, width, height):
-            self.__width = width
-            self.__height = height
+    """BaseGeography class"""
+
+    def area(self):
+        """raise exception"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """validates value"""
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+
+class Rectangle(BaseGeometry):
+    """Rectangle class"""
+
+    def __init__(self, width, height):
+        """initialize square size"""
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
