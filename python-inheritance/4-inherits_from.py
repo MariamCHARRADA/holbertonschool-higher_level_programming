db65a5c1a41478b329869c0d5cf63c066c5e41e2
MariamCHARRADA <charradamariam@yahoo.com>
2023-10-19 03:46:11 +0100

    remove long docstring

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -3,5 +3,4 @@
 
 
 def inherits_from(obj, a_class):
-    """returns True if the object is exactly an instance of the specified class"""
     return issubclass(type(obj), a_class) and type(obj) != a_class
