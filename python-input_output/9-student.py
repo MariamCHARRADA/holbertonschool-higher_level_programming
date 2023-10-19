33887bcc38d7bc2fd414b8a8fbd287508080c8f5
MariamCHARRADA <charradamariam@yahoo.com>
2023-10-19 19:05:20 +0100

    test

---
+++ python-input_output/9-student.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""creates an Object from a “JSON file”"""
+
+
+class Student:
+    """A class that defines a student"""
+
+    def __init__(self, first_name, last_name, age):
+        '''initializes a new Student instance'''
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+
+def to_json(self):
+    '''returns the JSON representation of a Student instance'''
+    return self.__dict__
