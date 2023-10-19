17800bd7ba322bb805aeac6b602eff48130ef477
MariamCHARRADA <charradamariam@yahoo.com>
2023-10-19 18:31:10 +0100

    format

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -3,6 +3,7 @@
 import sys
 from os.path import isfile
 
+
 save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
@@ -13,7 +14,7 @@ if isfile(filename):
     my_list = load_from_json_file(filename)
 else:
     my_list = []
-    
+
 my_list.append(args[1:])
 save_to_json_file(my_list, filename)
 print(my_list)
