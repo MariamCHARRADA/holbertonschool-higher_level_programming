36480aa33cacc2de11c7b9a9070fc82fe8d7247c
MariamCHARRADA <charradamariam@yahoo.com>
2023-10-19 01:57:55 +0100

    handle return

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -4,4 +4,4 @@
 
 def inherits_from(obj, a_class):
     """returns True if the object is exactly an instance of the specified class"""
-    issubclass(type(obj), a_class)
+    return issubclass(type(obj), a_class)
