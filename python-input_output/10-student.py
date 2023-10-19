f092df4d2c01bdca1a4fa3ae5d962658170aa24e
MariamCHARRADA <charradamariam@yahoo.com>
2023-10-19 19:28:03 +0100

    fix code

---
+++ python-input_output/10-student.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Write a class Student that defines a student”"""
+
+
+class Student:
+    """A class that defines a student"""
+
+    def __init__(self, first_name, last_name, age):
+        """initializes a new Student instance"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """returns the JSON representation of a Student instance"""
+        return self.__dict__
+
+    def to_json(self, attrs=None):
+        '''returns the JSON representation of a Student instance'''
+        if attrs is not None:
+            new_dict = {}
+            for key, value in self.__dict__.items():
+                if key in attrs:
+                    new_dict[key] = value
+            return new_dict
+        else:
+            return self.__dict__
