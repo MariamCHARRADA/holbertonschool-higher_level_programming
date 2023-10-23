60e5606570810f03b4020105c401896681a382c1
MariamCHARRADA <charradamariam@yahoo.com>
2023-10-23 16:10:28 +0100

    Base.__nb_objects

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -5,11 +5,11 @@
 class Base:
     """Base class"""
 
+    __nb_objects = 0
     def __init__(self, id=None):
         """Base class constructor"""
-        __nb_objects = 0
         if id is not None:
             self.id = id
         else:
-            __nb_objects += 1
-            self.id = __nb_objects
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
