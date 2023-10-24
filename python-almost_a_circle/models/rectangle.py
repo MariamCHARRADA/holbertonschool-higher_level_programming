65e5d829140b16e8d00f6690f51c622c769aa2f9
MariamCHARRADA <charradamariam@yahoo.com>
2023-10-24 22:04:42 +0100

    indent getters and setters properly

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -21,49 +21,49 @@ class Rectangle(Base):
         super().__init__(id)
 
 
-@property
-def width(self):
-    """width getter"""
-    return self.__width
+    @property
+    def width(self):
+        """width getter"""
+        return self.__width
 
 
-@width.setter
-def width(self, value):
-    """width setter"""
-    self.__width = value
+    @width.setter
+    def width(self, value):
+        """width setter"""
+        self.__width = value
 
 
-@property
-def height(self):
-    """height getter"""
-    return self.__height
+    @property
+    def height(self):
+        """height getter"""
+        return self.__height
 
 
-@height.setter
-def height(self, value):
-    """height setter"""
-    self.__height = value
+    @height.setter
+    def height(self, value):
+        """height setter"""
+        self.__height = value
 
 
-@property
-def x(self):
-    """x getter"""
-    return self.__x
+    @property
+    def x(self):
+        """x getter"""
+        return self.__x
 
 
-@x.setter
-def x(self, value):
-    """x setter"""
-    self.__x = value
+    @x.setter
+    def x(self, value):
+        """x setter"""
+        self.__x = value
 
 
-@property
-def y(self):
-    """y getter"""
-    return self.__y
+    @property
+    def y(self):
+        """y getter"""
+        return self.__y
 
 
-@y.setter
-def y(self, value):
-    """y setter"""
-    self.__y = value
+    @y.setter
+    def y(self, value):
+        """y setter"""
+        self.__y = value
