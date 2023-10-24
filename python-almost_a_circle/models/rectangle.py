717aa7b479f18cf4946208f0356acacaff44a9ec
MariamCHARRADA <charradamariam@yahoo.com>
2023-10-24 22:41:15 +0100

    if type not int

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -28,9 +28,7 @@ class Rectangle(Base):
     @width.setter
     def width(self, value):
         """width setter"""
-        try:
-            value = int(value)
-        except ValueError:
+        if type(value) is not int:
             raise TypeError("width must be an integer")
         if value <= 0:
             raise ValueError("width must be > 0")
@@ -44,9 +42,7 @@ class Rectangle(Base):
     @height.setter
     def height(self, value):
         """height setter"""
-        try:
-            value = int(value)
-        except ValueError:
+        if type(value) is not int:
             raise TypeError("height must be an integer")
         if value <= 0:
             raise ValueError("height must be > 0")
