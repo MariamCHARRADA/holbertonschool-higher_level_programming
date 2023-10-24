46695844f90a802121e672f4067531cf8ac71d01
MariamCHARRADA <charradamariam@yahoo.com>
2023-10-24 23:19:20 +0100

    update display using x, y

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -76,7 +76,11 @@ class Rectangle(Base):
 
     def display(self):
         """prints in stdout the rectangle with the character #"""
+        for a in range(self.__x):
+            print()
         for i in range(self.__height):
+            for b in range(self.__y):
+                print(" ", end="")
             for j in range(self.__width):
                 print("#", end="")
             print()
