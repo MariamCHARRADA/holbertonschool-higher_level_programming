b2c42a2eb74c1785dcc72bd772c71ecdda94b273
MariamCHARRADA <charradamariam@yahoo.com>
2023-10-25 13:54:10 +0100

    **square

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -10,14 +10,15 @@ class Square(Rectangle):
         """Square class constructor"""
         super().__init__(size, size, x, y, id)
 
+ 
     def __str__(self):
         """returns a string representation of the square"""
         return "[Square] ({}) {}/{} - {}".format(
             self.id, self.x, self.y, self.width
-            )
+            ) 
 
-    @property
-    def size(self):
+  @property
+   def size(self):
         """size getter"""
         return self.width
 
@@ -26,3 +27,26 @@ class Square(Rectangle):
         """size setter"""
         self.width = value
         self.height = value
+
+    def update(self, *args, **kwargs):
+        """assign arguments to attributes"""
+        if args:
+            for index, value in enumerate(args):
+                if index == 0:
+                    self.id = value
+                elif index == 1:
+                    self.size = value
+                elif index == 2:
+                    self.x = value
+                elif index == 3:
+                    self.y = value
+        else:
+            for key, value in kwargs.items():
+                if key == "id":
+                    self.id = value
+                elif key == "size":
+                    self.size = value
+                elif key == "x":
+                    self.x = value
+                elif key == "y":
+                    self.y = value
