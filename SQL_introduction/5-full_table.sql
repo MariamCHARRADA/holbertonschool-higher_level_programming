2f43469c595122fbf0ca91db41067198d4cb1875
MariamCHARRADA <charradamariam@yahoo.com>
2023-11-22 12:40:53 +0100

    ✨ Full table description

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,2 +1,2 @@
 --  print the description of the table first_table from the database hbtn_0c_0 in MySQL server
-IF EXISTS hbtn_0c_0.first_table SELECT * FROM hbtn_0c_0.first_table;
+SHOW CREATE TABLE first_table;
