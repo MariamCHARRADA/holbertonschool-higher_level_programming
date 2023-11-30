82c84b54a501637f2a17a7901f9737d844f3730a
MariamCHARRADA <charradamariam@yahoo.com>
2023-11-30 01:33:29 +0100

    if __name__ == "__main__":

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -4,6 +4,10 @@ import MySQLdb
 from sqlalchemy import create_engine
 from sys import argv
 
+if __name__ == "__main__":
+    """
+    A script that lists all states from the database hbtn_0e_0_usa
+    """
 engine = create_engine(
     "mysql+mysqldb://{}:{}@localhost/{}".format(argv[1], argv[2], argv[3])
 )
@@ -14,5 +18,6 @@ cursor.execute("SELECT * FROM states ORDER BY id ASC")
 rows = cursor.fetchall()
 for row in rows:
     print(row)
+
 cursor.close()
 connection.close()
