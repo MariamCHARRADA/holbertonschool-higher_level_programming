b66878cfba739a4f17e34b49ad93a015f956161a
MariamCHARRADA <charradamariam@yahoo.com>
2023-11-30 02:36:42 +0100

    name = '{}'

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -7,12 +7,14 @@ import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
-    '''Connect to MySQL database'''
-    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                        password=argv[2], db=argv[3])
+    """Connect to MySQL database"""
+    db = MySQLdb.connect(
+        host="localhost", port=3306, user=argv[1], password=argv[2], db=argv[3]
+    )
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM states WHERE name LIKE '{}' ORDER BY id"
-                .format(argv[4]))
+    cursor.execute(
+        "SELECT * FROM states WHERE name = '{}' ORDER BY id".format(argv[4])
+    )
     rows = cursor.fetchall()
     for row in rows:
         print(row)
