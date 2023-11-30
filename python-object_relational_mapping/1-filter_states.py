48c1bbb6b4d832cfe4cdeef67db95f3252315835
MariamCHARRADA <charradamariam@yahoo.com>
2023-11-30 02:46:38 +0100

    ✏️ fix typos

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -12,9 +12,9 @@ if __name__ == "__main__":
     db = MySQLdb.connect(
         user=argv[1], 
         password=argv[2], 
-        db=argv[3]
+        db=argv[3],
         host="localhost",
-        port=3306,
+        port=3306
     )
     cursor = db.cursor()
     cursor.execute("SELECT * FROM states WHERE BINARY name LIKE 'N%' ORDER BY id")
