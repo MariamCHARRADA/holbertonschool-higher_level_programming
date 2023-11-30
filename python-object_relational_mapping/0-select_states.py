e998c05c1d5ba3d7d662dde82c7ae8a7dbbc18ff
MariamCHARRADA <charradamariam@yahoo.com>
2023-11-30 01:53:17 +0100

    remove alchemy

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,18 +1,13 @@
 #!/usr/bin/python3
 """A script that lists all states from the database hbtn_0e_0_usa"""
 import MySQLdb
-from sqlalchemy import create_engine
 from sys import argv
 
 if __name__ == "__main__":
     """
     A script that lists all states from the database hbtn_0e_0_usa
     """
-engine = create_engine(
-    "mysql+mysqldb://{}:{}@localhost/{}".format(argv[1], argv[2], argv[3])
-)
-
-connection = engine.connect()
+connection = MySQLdb.connect(user=argv[1], password=argv[2], db=argv[3], port=3306)
 cursor = connection.cursor()
 cursor.execute("SELECT * FROM states ORDER BY id ASC")
 rows = cursor.fetchall()
