2162bff5dc214fbecc9d8de5fcd6613f607fc62a
MariamCHARRADA <charradamariam@yahoo.com>
2023-11-30 02:50:24 +0100

    format fix

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -8,6 +8,7 @@ from sys import argv
 
 if __name__ == "__main__":
     """Connect to MySQL database"""
+
     db = MySQLdb.connect(
         host="localhost", port=3306, user=argv[1], password=argv[2], db=argv[3]
     )
