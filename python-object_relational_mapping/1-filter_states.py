891a9344f0717548eb228a9971e2166b3c31f4ef
MariamCHARRADA <charradamariam@yahoo.com>
2023-11-30 02:45:00 +0100

    format fix

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -10,7 +10,11 @@ from sys import argv
 if __name__ == "__main__":
     """Connect to MySQL database"""
     db = MySQLdb.connect(
-        host="localhost", port=3306, user=argv[1], password=argv[2], db=argv[3]
+        user=argv[1], 
+        password=argv[2], 
+        db=argv[3]
+        host="localhost",
+        port=3306,
     )
     cursor = db.cursor()
     cursor.execute("SELECT * FROM states WHERE BINARY name LIKE 'N%' ORDER BY id")
