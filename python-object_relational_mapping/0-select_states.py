63cf74838f620b11ab630589113adb7a72afda6a
MariamCHARRADA <charradamariam@yahoo.com>
2023-11-30 01:28:08 +0100

    initial commit

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,17 @@
+"""A script that lists all states from the database hbtn_0e_0_usa"""
+import MySQLdb
+from sqlalchemy import create_engine
+from sys import argv
+
+engine = create_engine(
+    "mysql+mysqldb://{}:{}@localhost/{}".format(argv[1], argv[2], argv[3])
+)
+
+connection = engine.connect()
+cursor = connection.cursor()
+cursor.execute("SELECT * FROM states ORDER BY id ASC")
+rows = cursor.fetchall()
+for row in rows:
+    print(row)
+cursor.close()
+connection.close()
