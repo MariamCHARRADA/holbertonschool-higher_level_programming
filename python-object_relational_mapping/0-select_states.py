29965bbfeacbc8e6d967c6c2831f14592f648129
MariamCHARRADA <charradamariam@yahoo.com>
2023-11-30 02:08:26 +0100

    add host

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -5,7 +5,13 @@ from sys import argv
 
 if __name__ == "__main__":
     """Connect to MySQL database"""
-    connection = MySQLdb.connect(user=argv[1], password=argv[2], db=argv[3], port=3306)
+    connection = MySQLdb.connect(
+        host="localhost",
+        user=argv[1],
+        password=argv[2],
+        db=argv[3],
+        port=3306
+    )
     cursor = connection.cursor()
     cursor.execute("SELECT * FROM states ORDER BY id ASC")
     rows = cursor.fetchall()
