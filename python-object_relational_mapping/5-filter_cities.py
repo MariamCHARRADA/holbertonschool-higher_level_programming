da1450655655aef5273f703ad368562ff654ed45
MariamCHARRADA <charradamariam@yahoo.com>
2023-12-04 01:27:05 +0100

    fix cursor

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -11,11 +11,12 @@ if __name__ == "__main__":
     )
     cursor = db.cursor()
     cursor.execute(
-        """SELECT cities.id, cities.name, states.name
-        FROM cities
-        INNER JOIN states ON cities.state_id = states.id
-        WHERE states.name = %s
-        ORDER BY cities.id, (argv[4])"""
+        "SELECT cities.id, cities.name, states.name"
+        "FROM cities"
+        "INNER JOIN states ON cities.state_id = states.id"
+        "WHERE states.name = %s"
+        "ORDER BY cities.id, (argv[4])"
+        (argv[4],)
     )
     rows = cursor.fetchall()
     for row in rows:
