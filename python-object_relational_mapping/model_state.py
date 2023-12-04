03c2f9359ccb6ac833c8bd89805d573165fa7785
MariamCHARRADA <charradamariam@yahoo.com>
2023-12-04 01:48:59 +0100

    format fix

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,4 +1,4 @@
-Write a python file that contains the class definition of a State and an instance Base = declarative_base()
+"""Write a python file that contains the class definition of a State and an instance Base = declarative_base()"""
 
 import sqlalchemy
 from sqlalchemy import Column, Integer, String
@@ -6,9 +6,12 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
-class State(Base):
-    '''State class'''
 
-    __tablename__ = 'states'
-    id = Column(Integer, primary_key=True, nullable=False, unique=True, autoincrement=True)
+class State(Base):
+    """State class"""
+
+    __tablename__ = "states"
+    id = Column(
+        Integer, primary_key=True, nullable=False, unique=True, autoincrement=True
+    )
     name = Column(String(128), nullable=False)
