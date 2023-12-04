888f57712a3e52e8225c85176d915a7d1410e8c6
MariamCHARRADA <charradamariam@yahoo.com>
2023-12-04 01:59:02 +0100

    requirement fix

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -12,7 +12,5 @@ class State(Base):
     """State class"""
 
     __tablename__ = "states"
-    id = Column(
-        Integer, primary_key=True, nullable=False, unique=True, autoincrement=True
-    )
+    id = Column(Integer, primary_key=True, autoincrement=True)
     name = Column(String(128), nullable=False)
